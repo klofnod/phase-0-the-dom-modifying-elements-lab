49f0449d87777ddde9ca6cb54185775ec6c6bcef
Jeremy <jeremy__francis@hotmail.com>
2022-05-05 13:33:39 -0700

    commit tree c645c34b50d43eafde4c09af5a459c321c0de476

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const get=document.querySelector("#main")
+get.remove()
+const newHeader= document.createElement('h1') 
+newHeader.setAttribute ('id', 'victory')
+newHeader.textContent= 'klofnod is the champion'
+document.body.append(newHeader)
